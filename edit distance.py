254e41cd3d25c7ef72ad235b6c0ec559ae2b34c9
Jagmohan Singh <jagmohan.singh@Mycity4kids.com>
2016-11-20 23:59:44 +0530

    edit distance with the steps

--- edit distance.py
+++ edit distance.py
@@ -1,21 +1,18 @@
-from collections import defaultdict
+global edit_table
+edit_table = list()
 
-
-def init_edit_table(original_string_length,final_string_length ):
-    edit_table = []
-    print edit_table
+# function to initialize edit table
+def init_edit_table(original_string_length,final_string_length):
     edit_table.append([x for x in xrange(original_string_length + 1)])
     for i in xrange(1, final_string_length + 1):
         edit_table.append([i] + [0] * original_string_length)
-    return edit_table
 
 
-# def get
-
+# creating the table for number of edits
 def create_edit_table(original_string,final_string):
     final_string_length = len(final_string)
     original_string_length = len(original_string)
-    edit_table = init_edit_table(original_string_length,final_string_length)
+    init_edit_table(original_string_length,final_string_length)
     for original_string_counter in xrange(original_string_length):
         for final_string_counter in xrange(final_string_length):
             if original_string[original_string_counter]==final_string[final_string_counter]:
@@ -25,13 +22,35 @@ def create_edit_table(original_string,final_string):
 
 
 
-    return edit_table
-    # table = [final_string_length+1][original_string_length+1]
-    # pass
+    return edit_table[final_string_length][original_string_length]
 
 
-edit_table = create_edit_table('abcdef','azced')
-def create_edit_characters(edit_table):
-    print
-for x in edit_table:
-    print x
+# function to get the steps to convert the original string to final string from end of strings
+def create_edit_characters(original_string,final_string):
+    final_string_length = len(final_string)
+    character_index_at_original = len(original_string)
+    character_table=[]
+    character_index_at_final = final_string_length
+    while character_index_at_final>0 :
+        if edit_table[character_index_at_final][character_index_at_original]==edit_table[character_index_at_final-1][character_index_at_original-1] and original_string[character_index_at_original-1]==final_string[character_index_at_final-1]:
+            character_index_at_final -=1
+            character_index_at_original -=1
+        elif edit_table[character_index_at_final][character_index_at_original]-1==edit_table[character_index_at_final-1][character_index_at_original]:
+            character_table.append([final_string[character_index_at_final-1],'ADD'])
+            character_index_at_final -=1
+        elif edit_table[character_index_at_final][character_index_at_original]-1==edit_table[character_index_at_final][character_index_at_original-1]:
+            character_table.append([original_string[character_index_at_original-1],'DELETE'])
+            character_index_at_original -=1
+        elif edit_table[character_index_at_final][character_index_at_original]-1==edit_table[character_index_at_final-1][character_index_at_original-1]:
+            character_table.append([original_string[character_index_at_original-1],final_string[character_index_at_final-1], 'MODIFY'])
+            character_index_at_final -=1
+            character_index_at_original -=1
+            # if final_string[character_index_at_final-1]==original_string[character_index_at_original-1]:
+
+    print character_table
+
+original_string = 'aunday'
+final_string = 'saturday'
+number_of_edit = create_edit_table(original_string,final_string)
+print number_of_edit
+create_edit_characters(original_string,final_string)
